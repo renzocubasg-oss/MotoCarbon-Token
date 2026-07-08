69e85107fa51d5f2ffc52b48d6860e705be0c7a0
renzocubasg-oss <renzo.cubasg@unmsm.edu.pe>
2026-07-08 03:24:07 -0500

    Add Hardhat configuration

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    lacchain: {
+      url: "http://35.193.217.67",
+      accounts: []
+    }
+  }
+};
